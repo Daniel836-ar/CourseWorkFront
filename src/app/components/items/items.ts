f1be722cd8b15c411c6b508d0bbe794aea70f7a9
Danil <dhivnicenko@gmail.com>
2025-12-05 19:19:07 +0700

    сделал вывод в несколько колонок , добавил аудио, и чуть-чуть поигрался со стилями

--- src/app/components/items/items.ts
+++ src/app/components/items/items.ts
@@ -12,9 +12,23 @@ import { CommonModule } from '@angular/common';
 })
 export class Items {
   allItems: SoundItem[] = [
-    new SoundItem("Как дела ?", "/assets/sounds/kakDela.mp3"),
     new SoundItem("Привет !", "/assets/sounds/privetDrug.mp3"), 
-    new SoundItem("Как неожиданно !", "/assets/sounds/votEtoPovorot.mp3")
+    new SoundItem("Уважительное здраствуйте всем ", "/assets/sounds/zdravstvuite.mp3"),
+    new SoundItem("Дружественное привет всем", "/assets/sounds/zdarova-bandity.mp3"),
+    new SoundItem("Всего хорошего !", "/assets/sounds/VsegoHoroshego.mp3"),
+    new SoundItem("Как дела ?", "/assets/sounds/kakDela.mp3"),
+    new SoundItem("Как неожиданно !", "/assets/sounds/votEtoPovorot.mp3"),
+    new SoundItem("Я не уверен", "/assets/sounds/etoNeTochno.mp3"),
+    new SoundItem("Как же грустно ...", "/assets/sounds/sadMusic.mp3"),
+    new SoundItem("Очень смешно !", "/assets/sounds/smeshno.mp3"),
+
+    new SoundItem("Я крайне вами недоволоден !", "/assets/sounds/250tis.mp3"),
+    new SoundItem("Я не понял", "/assets/sounds/error.mp3"), 
+    new SoundItem("Зачем этот нехороший человек снова пришёл ?", "/assets/sounds/fu-fu-fu.mp3"),
+    new SoundItem("Мне кажется ты глуп", "/assets/sounds/otklonenia.mp3"),
+    new SoundItem("Вау !", "/assets/sounds/wow.mp3"),
+
+    new SoundItem("Зря я туда пошёл", "/assets/sounds/zria.mp3")
   ];
   title: string = "Крутая подсказка"; 
   
